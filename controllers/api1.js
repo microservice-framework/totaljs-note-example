a338b42e94e462cd21005008f5b0f449aed551c5
Alexey Kolosov <softovick@gmail.com>
2018-03-04 20:44:40 +0300

    add find by name

--- controllers/api1.js
+++ controllers/api1.js
@@ -1,6 +1,6 @@
 exports.install = function() {
 	F.route('v1/users', users1);
-
+    F.route('v1/users/{name}', users1_name);
 	// or
 	// F.route('/');
 };
@@ -9,6 +9,14 @@ function users1() {
 	var self = this;
 	var users = DATABASE('users');
 	users.find({}).toArray(function (err, docs) {
+		self.json(docs);
+	});
+}
+
+function users1_name(name) {
+    var self = this;
+	var users = DATABASE('users');
+	users.find({"login": name}).toArray(function (err, docs) {
 		self.json(docs);
 	});
 }
