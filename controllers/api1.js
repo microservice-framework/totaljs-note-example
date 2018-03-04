4e0f8fac76838f467a732e0e8accdbd04eb3baa9
Alexey Kolosov <softovick@gmail.com>
2018-03-05 01:57:09 +0300

    add prototype POST user

--- controllers/api1.js
+++ controllers/api1.js
@@ -1,6 +1,7 @@
-exports.install = function() {
+exports.install = function () {
 	F.route('v1/users', users1);
-    F.route('v1/users/{name}', users1_name);
+	F.route('v1/users/{name}', users1_find_name);
+	F.route('v1/users', users1_create, ['post']);
 	// or
 	// F.route('/');
 };
@@ -13,10 +14,18 @@ function users1() {
 	});
 }
 
-function users1_name(name) {
-    var self = this;
+function users1_find_name(name) {
+	var self = this;
 	var users = DATABASE('users');
-	users.find({"login": name}).toArray(function (err, docs) {
+	users.find({
+		"login": name
+	}).toArray(function (err, docs) {
 		self.json(docs);
 	});
+}
+
+function users1_create() {
+	var self = this;
+	console.log(self.body);
+	self.json(self.body.name);
 }
